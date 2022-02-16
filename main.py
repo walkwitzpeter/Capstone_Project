e2047ae1597b45136260d953b9830db7c6548ddb
Peter Walkwitz <walkwitzpeter@gmail.com>
2022-02-15 20:01:49 -0600

    All modes now work with unduplicated code in a class

--- main.py
+++ main.py
@@ -8,19 +8,7 @@ import google_api
 import record_audio
 import tkinter_window
 
-# TODO possibilities
 import word_image_dictionary
-
-'''
-Get list of highest probability words from Google API
-'''
-
-# Ideas for "game modes"
-'''
-Type say see - user types the word (from a picture) then says the word (after they typed it right) then sees the word 
-Basic association - sees a photo says the word (start with this?)
-
-'''
 
 
 def open_homepage_window(prev_win=None):
@@ -46,19 +34,7 @@ def open_homepage_window(prev_win=None):
     associations_btn.grid(row=0, rowspan=2, column=0, columnspan=2)
     type_say_see_btn.grid(row=1, rowspan=2, column=0, columnspan=2)
 
-    # Making my buttons to record audio
-    # exit_button = Button(window, text="Exit", command=lambda: window.destroy())
-    # record_audio_btn = Button(window, text="Start Recording", command=lambda: record_audio.RecAUD())
-    # see_audio_text_btn = Button(window, text="See Audio Text", command=lambda: google_api.GoogleAPI().get_transcript())
-    # play_audio_btn = Button(window, text="Play Audio", command=lambda: play_audio())
-    #
-    # # Putting my buttons on the screen, and configuring the Grid
-    # record_audio_btn.grid(row=0, rowspan=2, column=0, columnspan=2)
-    # exit_button.grid(row=2, rowspan=2, column=1, columnspan=2)
-    # see_audio_text_btn.grid(row=0, rowspan=1, column=3, columnspan=2)
-    # play_audio_btn.grid(row=1, rowspan=1, column=3, columnspan=2)
-    #
-    # # Organizing the grid for my button layout
+    # Organizing the grid for my button layout
     for row_number in range(3):
         Grid.rowconfigure(window, row_number, weight=1)
     # for column_number in range(4):
@@ -68,120 +44,62 @@ def open_homepage_window(prev_win=None):
 
 
 def open_basic_association(prev_win):
-    # Grid size variables
-    COLUMNS = 8
-    ROWS = 8
-    # Tk variables
+    # Running my Basic Game Window
     tk = tkinter_window.TkInterWindow()
     tk.open_new_window(prev_win, "Basic Associations")
-    main = tk.window
-    # Loading right answer and wrong answer pictures
-    red_x = PhotoImage(file="Photos/Red X.png").subsample(9, 9)
-    green_check = PhotoImage(file="Photos/Green Check.png").subsample(9, 9)
-
-    # Creating something to keep track of what the current answer is and load photos
-    photos_and_answers = word_image_dictionary.Words()
-    photos_and_answers.initializeStacks()
-
-    # Making my grid to place items
-    for row_number in range(ROWS):
-        Grid.rowconfigure(main, row_number, weight=2)
-    for column_number in range(COLUMNS):
-        Grid.columnconfigure(main, column_number, weight=2)
-
-    # Setting up the first image/answer for the user
-    img = photos_and_answers.imgStack.pop()
-    photos_and_answers.updateCurAnswer()
-    main_img = Label(main, image=img)
-    main_img.grid(row=1, rowspan=2, column=3, columnspan=3)
-
-    # Setting up the record button for the user
-    record_audio_btn = Button(main, text="Record Answer", command=lambda: record_audio.RecAUD())
-    record_audio_btn.grid(row=4, column=4)
-    # Seeing the recording text
-    recording_text = Label(main, text="Your answer appears here", compound=LEFT)
-    recording_text.grid(row=5, column=4, columnspan=2)
-    check_recording_btn = Button(main, text="Check Recording",
-                                 command=lambda: check_recording(recording_text, photos_and_answers.curAnswer,
-                                                                 green_check, red_x))
-    check_recording_btn.grid(row=4, column=5)
-    # Hiding the check recording button until user records something
-    # check_recording_btn.grid_remove()
-
-    # Placing my back and next arrows
-    back_arw_img = tkinter.PhotoImage(master=main, file=r"Photos/Back Arrow.png").subsample(7, 7)
-    back_btn = Button(main, text="Back", command=lambda: open_homepage_window(main), image=back_arw_img)
-    back_btn.grid(row=0, column=0)
-    next_arw_img = tkinter.PhotoImage(master=main, file=r"Photos/Next Arrow.png").subsample(9, 9)
-    next_arw_btn = Button(main, text="Next",
-                          command=lambda: update_image(main_img, photos_and_answers.imgStack, photos_and_answers,
-                                                       recording_text),
-                          image=next_arw_img)
-    next_arw_btn.grid(row=ROWS, column=COLUMNS)
-
-    # Assigning back/next button rows and columns to be smaller than the rest
-    Grid.rowconfigure(main, 0, weight=1)
-    Grid.columnconfigure(main, 0, weight=1)
-    Grid.rowconfigure(main, ROWS, weight=1)
-    Grid.columnconfigure(main, COLUMNS, weight=1)
-
-    main.mainloop()
+    tk.window.mainloop()
 
 
 def open_type_say_see(prev_win):
-    # Grid size variables
-    COLUMNS = 8
-    ROWS = 8
+#     # Grid size variables
+#     COLUMNS = 8
+#     ROWS = 8
     # Tk Variables
     tk = tkinter_window.TkInterWindow()
     tk.open_new_window(prev_win, "Type Say See Hear")
-    main = tk.window
-    # Setting up answer images
-    red_x = PhotoImage(file="Photos/Red X.png").subsample(9, 9)
-    green_check = PhotoImage(file="Photos/Green Check.png").subsample(9, 9)
+#     main = tk.window
+#     # Setting up answer images
+#     red_x = PhotoImage(file="Photos/Red X.png").subsample(9, 9)
+#     green_check = PhotoImage(file="Photos/Green Check.png").subsample(9, 9)
+#
+#     # Creating something to keep track of what the current answer is and load photos
+#     photos_and_answers = word_image_dictionary.Words()
+#     photos_and_answers.initializeStacks()
+#
+#     # Making my grid to place items
+#     for row_number in range(ROWS):
+#         Grid.rowconfigure(main, row_number, weight=2)
+#     for column_number in range(COLUMNS):
+#         Grid.columnconfigure(main, column_number, weight=2)
+#
+#     # Setting up the first image/answer for the user
+#     img = photos_and_answers.imgStack.pop()
+#     photos_and_answers.updateCurAnswer()
+#     main_img = Label(main, image=img)
+#     main_img.grid(row=1, rowspan=2, column=3, columnspan=3)
+#
+#     # TODO
+    # Set up the input typing
+    entry = Entry(tk.window, width=50)
+    entry.focus_set()
+    entry.grid(row=3, column=3, columnspan=4)
+    check_entry_btn = Button(tk.window, text="Check Answer",
+                             command=lambda: tk.check_text_entry(entry))
+    check_entry_btn.grid(row=5, column=3, columnspan=2)
 
-    img = tkinter.PhotoImage(file="Photos/mic_image.png", master=main)
-    label = Label(
-        main,
-        image=img,
-        height=main.winfo_screenheight(),
-        width=main.winfo_screenwidth()
-    )
-    label.place(x=0, y=0, relwidth=1, relheight=1)
+    # Seeing the correct answer
+    see_answer_btn = Button(tk.window, text="See Answer",
+                            command=lambda: tk.see_answer())
+    see_answer_btn.grid(row=5, column=5, columnspan=2)
 
-    back_btn = Button(main, text="Back to Homepage", command=lambda: open_homepage_window(main))
-    back_btn.grid(row=0, rowspan=1, column=0, columnspan=1)
-
-
-def check_recording(recording_text, cur_answer, check, red_x):
-    if cur_answer != "Done":
-        text = str(google_api.GoogleAPI().get_transcript())
-        recording_text['text'] = "We heard the following possibilities:\n" + text
-        recording_text.grid()
-        print("cur_answer: " + cur_answer)
-        if text.casefold().__contains__(cur_answer.casefold()):
-            recording_text['image'] = check
-            print("found")
-        else:
-            recording_text['image'] = red_x
-            print("not found")
-    else:
-        recording_text['text'] = "You went through all the words!"
-        recording_text.grid()
-
-
-def update_image(label, img_stack, photos_and_answers, recording_text):
-    # Changing image if there is another image waiting and update answer
-    if img_stack:
-        img = img_stack.pop()
-        label.configure(image=img)
-        label.image = img
-        photos_and_answers.updateCurAnswer()
-        recording_text.grid_remove()
+    # Hiding the buttons I don't want shown
+    tk.record_audio_btn.grid_remove()
+    tk.check_recording_btn.grid_remove()
 
 
 if __name__ == '__main__':
     open_homepage_window()
+
 
 # Sources (description below the links)
 # https://realpython.com/playing-and-recording-sound-python/#recording-audio
@@ -191,6 +109,7 @@ if __name__ == '__main__':
 # https://www.youtube.com/watch?v=NytF3pJSMc8&ab_channel=Codemy.com
 # https://pythonprogramming.altervista.org/python-gui-with-tkinter-labels-with-text-and-images/?doing_wp_cron=1644523669.3052639961242675781250
 # https://www.delftstack.com/howto/python-tkinter/how-to-hide-recover-and-delete-tkinter-widgets/#grid-remove-method-to-hide-tkinter-widgets-if-grid-layout-is-used
+# https://www.tutorialspoint.com/taking-input-from-the-user-in-tkinter#:~:text=We%20can%20get%20the%20user,help%20of%20the%20Label%20widget
 #     Used for TKinter basics (And other Codemy.com videos on youtube)
 # https://www.geeksforgeeks.org/python-add-image-on-a-tkinter-button/
 # https://twitter.com/humanbrainproj/status/1233727365768974336?lang=eu
--- tkinter_window.py
+++ tkinter_window.py
@@ -1,19 +1,133 @@
 from tkinter import *
+import tkinter
 
+import google_api
 import main
 import record_audio
+import word_image_dictionary
 
 
 class TkInterWindow:
     def __init__(self):
+        # Constants for my grid
+        self.ROWS = 8
+        self.COLUMNS = 8
+
+        # Setting up the different variables for my window
         self.window = None
+        self.feedback_text = None
+
+        # Buttons
         self.back_btn = None
+        self.next_arw_btn = None
+        self.record_audio_btn = None
+        self.check_recording_btn = None
+
+        # Image
+        self.back_arw_img = None
+        self.next_arw_img = None
+        self.main_img = None
+        self.check_img = None
+        self.x_img = None
+
+        # Answer/Img Stack
+        self.photos_and_answers = None
+
+    #TODO
+    # Separate all of the words into different categories
+    # Click on what category you want after selecting the game mode? or before?
 
     def open_new_window(self, prev_win, title):
         prev_win.destroy()
         self.window = Tk()
         self.window.title(title)
         self.window.geometry("800x500")
+
+        # Correct and Incorrect Feedback Images
+        self.x_img = PhotoImage(file="Photos/Red X.png").subsample(9, 9)
+        self.check_img = PhotoImage(file="Photos/Green Check.png").subsample(9, 9)
+
+        # Creating something to keep track of what the current answer is and load photos
+        self.photos_and_answers = word_image_dictionary.Words()
+        self.photos_and_answers.initializeStacks()
+
+        # Making my grid to place items
+        for row_number in range(self.ROWS):
+            Grid.rowconfigure(self.window, row_number, weight=2)
+        for column_number in range(self.COLUMNS):
+            Grid.columnconfigure(self.window, column_number, weight=2)
+
+        # Setting up the main image
+        img = self.photos_and_answers.imgStack.pop()
+        self.photos_and_answers.updateCurAnswer()
+        self.main_img = Label(self.window)
+        self.main_img.configure(image=img)
+        self.main_img.image = img
+        self.main_img.grid(row=1, rowspan=2, column=3, columnspan=3)
+
+        # Placing the feedback text box
+        self.feedback_text = Label(self.window, text="Your answer appears here", compound=LEFT)
+        self.feedback_text.grid(row=6, column=4, columnspan=2)
+
+        # Placing my back and next arrows
+        self.back_arw_img = PhotoImage(master=self.window, file=r"Photos/Back Arrow.png").subsample(7, 7)
+        self.back_btn = Button(self.window, text="Back",
+                               command=lambda: main.open_homepage_window(self.window),
+                               image=self.back_arw_img)
+        self.back_btn.grid(row=0, column=0)
+        self.next_arw_img = PhotoImage(master=self.window, file=r"Photos/Next Arrow.png").subsample(9, 9)
+        self.next_arw_btn = Button(self.window, text="Next",
+                                   command=lambda: self.update_image(),
+                                   image=self.next_arw_img)
+        self.next_arw_btn.grid(row=self.ROWS, column=self.COLUMNS)
+
+        # Setting up the record button
+        self.record_audio_btn = Button(self.window, text="Record Answer",
+                                       command=lambda: record_audio.RecAUD())
+        self.record_audio_btn.grid(row=4, column=4)
+        # Checking the recording
+        self.check_recording_btn = Button(self.window, text="Check Recording",
+                                          command=lambda: self.check_recording())
+        self.check_recording_btn.grid(row=4, column=5)
+
+    def see_answer(self):
+        self.feedback_text['text'] = self.photos_and_answers.curAnswer
+        self.feedback_text['image'] = ""
+        self.feedback_text.grid()
+
+    def check_text_entry(self, user_input):
+        user_text = user_input.get()
+        if user_text == self.photos_and_answers.curAnswer:
+            self.feedback_text['text'] = "Correct!"
+            self.feedback_text['image'] = self.check_img
+            self.record_audio_btn.grid()
+            self.check_recording_btn.grid()
+        else:
+            self.feedback_text['text'] = "Incorrect"
+            self.feedback_text['image'] = self.x_img
+
+    def update_image(self):
+        # Changing image if there is another image waiting and update answer
+        if self.photos_and_answers.imgStack:
+            img = self.photos_and_answers.imgStack.pop()
+            self.main_img.configure(image=img)
+            self.main_img.image = img
+            self.photos_and_answers.updateCurAnswer()
+            self.feedback_text.grid_remove()
+
+    def check_recording(self):
+        if self.photos_and_answers.curAnswer != "Done":
+            text = str(google_api.GoogleAPI().get_transcript())
+            self.feedback_text['text'] = "We heard the following possibilities:\n" + text
+            self.feedback_text.grid()
+            print("cur_answer: " + self.photos_and_answers.curAnswer)
+            if text.casefold().__contains__(self.photos_and_answers.curAnswer.casefold()):
+                self.feedback_text['image'] = self.check_img
+            else:
+                self.feedback_text['image'] = self.x_img
+        else:
+            self.feedback_text['text'] = "You went through all the words!"
+            self.feedback_text.grid()
 
     def close_window(self):
         self.window.destroy()
